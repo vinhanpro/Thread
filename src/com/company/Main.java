48c557ca050ac5d56c01570024a316a3e505a98e
Điền Trần Vĩnh An <dienvinhan@gmail.com>
2021-05-14 10:31:55 +0700

    submit(Runnable)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,37 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
 
 
-        public static void main(String[] args) {
-            // Khai báo một Thread Pool thông qua newSingleThreadExecutor() của Executors
-            ExecutorService executorService = Executors.newCachedThreadPool();
-            // Khai báo 10 Runnable, và "quăng" chúng vào Thread Pool vừa khai báo
-            for (int i = 1; i <= 5; i++) {
-                MyRunnable myRunnable = new MyRunnable("Runnable " + i);
-                executorService.execute(myRunnable);
-            }
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        List<Future> listFuture = new ArrayList<Future>(); // Khởi tạo danh sách các Future
 
-            // Phương thức này sẽ được nói sau ở ExecutorService
-            executorService.shutdown();
+        for (int i = 1; i <= 10; i++) {
+            MyRunnable myRunnable = new MyRunnable("Runnable " + i);
+            // Bước này chúng ta dùng submit() thay vì execute()
+            Future future = executorService.submit(myRunnable);
+            listFuture.add(future); // Từng Future sẽ quản lý một Runnable
         }
 
+        for (Future future : listFuture) {
+            try {
+                // Khi Thread nào kết thúc, get() của Future tương ứng sẽ trả về null
+                System.out.println(future.get());
+            } catch (ExecutionException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        // Phương thức này đã nói ở trên đây rồi
+        executorService.shutdown();
+    }
 }
